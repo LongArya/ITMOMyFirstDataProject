b11f2d2931d241307776326836248afcc5bf827b
Andrey Shilkov <a.shilkov@3divi.com>
2023-03-21 13:31:06 +0500

    added data structure for static gesture

---
+++ static_gesture_classification/static_gesture.py
@@ -0,0 +1,14 @@
+from enum import Enum, auto
+
+
+class StaticGesture(Enum):
+    BACKGROUND = auto()
+    ONE = auto()
+    TWO = auto()
+    THREE = auto()
+    FOUR = auto()
+    FIVE = auto()
+    THUMBS_UP = auto()
+    THUMBS_DOWN = auto()
+    OKEY = auto()
+    FIST = auto()
