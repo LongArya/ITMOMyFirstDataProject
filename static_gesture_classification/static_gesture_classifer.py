f1228edf8519c686c96b17ebf22ae61e22808d71
Andrey Shilkov <a.shilkov@3divi.com>
2023-04-11 02:40:48 +0500

    WIP neptune metrics

--- static_gesture_classification/static_gesture_classifer.py
+++ static_gesture_classification/static_gesture_classifer.py
@@ -1,4 +1,6 @@
 from torchvision.models import resnet18
+from neptune.types import File
+import matplotlib.pyplot as plt
 from dataclasses import dataclass
 import os
 import numpy as np
@@ -13,26 +15,15 @@ from typing import Dict, List
 from collections import defaultdict
 from general.data_structures.data_split import DataSplit
 from static_gesture_classification.static_gesture import StaticGesture
-
-
-# TODO move to other place
-@dataclass
-class ClassificationResultsDataframe(pd.DataFrame):
-    @property
-    def image_path(self) -> str:
-        ...
-
-    @property
-    def ground_true(self) -> str:
-        ...
-
-    @property
-    def prediction(self) -> str:
-        ...
-
-    @property
-    def prediction_score(self) -> float:
-        ...
+from static_gesture_classification.classification_results_dataframe import (
+    ClassificationResultsDataframe,
+)
+from static_gesture_classification.metrics_utils import (
+    compute_pr_curve_for_gesture,
+    get_pr_curves_for_gestures,
+    get_pr_curve_plot,
+    generate_confusion_matrix_plot_from_classification_results,
+)
 
 
 def init_static_gesture_classifier(cfg: StaticGestureConfig) -> nn.Module:
@@ -59,7 +50,13 @@ def init_lr_scheduler(optimizer, cfg: StaticGestureConfig):
         )
 
 
-# Metrics calculations -> requires train and val predictions?
+def init_classification_results_dataframe() -> ClassificationResultsDataframe:
+    return pd.DataFrame(
+        columns=["image_path", "ground_true", "prediction", "prediction_score"]
+        + [gesture.name for gesture in StaticGesture]
+    )
+
+
 class StaticGestureClassifier(pl.LightningModule):
     def __init__(
         self,
@@ -73,11 +70,7 @@ class StaticGestureClassifier(pl.LightningModule):
         self.model = init_static_gesture_classifier(self.cfg)
         self.predictions_on_datasets: Dict[
             DataSplit, ClassificationResultsDataframe
-        ] = defaultdict(
-            lambda: pd.DataFrame(
-                columns=["image_path", "ground_true", "prediction", "prediction_score"]
-            )
-        )
+        ] = defaultdict(init_classification_results_dataframe)
         self.results_location = results_location
 
     def _append_predictions_to_split(
@@ -93,15 +86,19 @@ class StaticGestureClassifier(pl.LightningModule):
         predictions_scores = torch.take_along_dim(probs, pred_classes, dim=1)
 
         batch_predictions = []
-        for path, gt_class, pred_class, pred_score in zip(
-            images_paths, gt_classes, pred_classes, predictions_scores
+        for path, gt_class, pred_class, pred_score, single_image_probabilities in zip(
+            images_paths, gt_classes, pred_classes, predictions_scores, probs
         ):
-            gt_label = StaticGesture(gt_class.item())
-            pred_label = StaticGesture(pred_class.item())
-            batch_predictions.append([path, gt_label, pred_label, pred_score.item()])
+            gt_label = StaticGesture(gt_class.item()).name
+            pred_label = StaticGesture(pred_class.item()).name
+            # TODO append scores of all classes
+            batch_predictions.append(
+                [path, gt_label, pred_label, pred_score.item()]
+                + [prob.item() for prob in single_image_probabilities]
+            )
         batch_prediction_dataframe: ClassificationResultsDataframe = pd.DataFrame(
             batch_predictions,
-            columns=["image_path", "ground_true", "prediction", "prediction_score"],
+            columns=self.predictions_on_datasets[split].columns,
         )
         self.predictions_on_datasets[split] = pd.concat(
             [self.predictions_on_datasets[split], batch_prediction_dataframe],
@@ -121,8 +118,9 @@ class StaticGestureClassifier(pl.LightningModule):
         )
 
         loss = self.criterion(pred_labels, gt_labels)
-        # TODO specify batch size, since in might be incorrectly inferred from dict
-        self.log("train_loss", loss)
+        # specify batch size, since in might be incorrectly inferred from dict by lightning
+        batch_size = batch["image"].size(dim=0)
+        self.log("train_loss", loss, batch_size=batch_size)
         return loss
 
     def validation_step(self, batch, batch_idx):
@@ -146,21 +144,41 @@ class StaticGestureClassifier(pl.LightningModule):
         )
         self.predictions_on_datasets[DataSplit.TRAIN].to_csv(save_path)
         # refresh results
-        self.predictions_on_datasets[DataSplit.TRAIN] = pd.DataFrame(
-            columns=["ground_true", "prediction", "image_path"]
-        )
+        self.predictions_on_datasets[
+            DataSplit.TRAIN
+        ] = init_classification_results_dataframe()
         return super().on_train_epoch_end()
 
+    def _log_conf_matrix_to_neptune(
+        self,
+        classification_results: ClassificationResultsDataframe,
+        log_path: str,
+    ) -> None:
+        fig, ax = plt.subplots()
+        ax = generate_confusion_matrix_plot_from_classification_results(
+            classification_results, ax
+        )
+        self.logger.experiment[log_path].upload(File.as_image(fig))
+        plt.close(fig)
+
     def on_validation_epoch_end(self) -> None:
-        # save results
         save_path = os.path.join(
             self.results_location.val_predictions_root, f"{self.current_epoch:04d}.csv"
         )
-        self.predictions_on_datasets[DataSplit.VAL].to_csv(save_path)
-        # refresh results
-        self.predictions_on_datasets[DataSplit.VAL] = pd.DataFrame(
-            columns=["ground_true", "prediction", "image_path"]
+        conf_matrix_neptune_path = (
+            f"val/figures/confusion_matrices/{self.current_epoch:04d}"
         )
+        self._log_conf_matrix_to_neptune(
+            classification_results=self.predictions_on_datasets[DataSplit.VAL],
+            log_path=conf_matrix_neptune_path,
+        )
+        self.predictions_on_datasets[DataSplit.VAL].to_csv(save_path)
+
+        # refresh results
+        self.predictions_on_datasets[
+            DataSplit.VAL
+        ] = init_classification_results_dataframe()
+
         return super().on_validation_epoch_end()
 
     def configure_optimizers(self):
@@ -170,4 +188,11 @@ class StaticGestureClassifier(pl.LightningModule):
             momentum=self.cfg.train_hyperparams.momentun,
         )
         scheduler = init_lr_scheduler(optimizer=optimizer, cfg=self.cfg)
-        return {"optimizer": optimizer, "lr_scheduler": scheduler, "monitor": "loss"}
+        return {
+            "optimizer": optimizer,
+            "lr_scheduler": scheduler,
+            "monitor": "train_loss",
+        }
+
+
+# failure cases
