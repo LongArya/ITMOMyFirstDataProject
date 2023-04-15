d1d24189c3532dfe8816ac3b431f1cbba5163d1b
Andrey Shilkov <a.shilkov@3divi.com>
2023-04-16 00:59:37 +0500

    added more paths for custom data

--- const.py
+++ const.py
@@ -6,6 +6,11 @@ REPO_DIR = os.path.dirname(WORKSPACE_DIR)
 DATA_ROOT = os.path.join(WORKSPACE_DIR, "Data")
 NDRCZC_DATA_ROOT = os.path.join(DATA_ROOT, "ndrczc35bt-1")
 CUSTOM_DATA_ROOT = os.path.join(DATA_ROOT, "custom_data")
+CUSTOM_TRAIN_ROOT = os.path.join(CUSTOM_DATA_ROOT, "train")
+CUSTOM_VAL_ROOT = os.path.join(CUSTOM_DATA_ROOT, "val")
+CUSTOM_VAL_VIZ_ROOT = os.path.join(CUSTOM_DATA_ROOT, "val_viz")
+CUSTOM_TRAIN_VIZ_ROOT = os.path.join(CUSTOM_DATA_ROOT, "train_viz")
+CUSTOM_PRESPLIT_ROOT = os.path.join(CUSTOM_DATA_ROOT, "presplit_data")
 
 TRAIN_RESULTS_ROOT = os.path.join(WORKSPACE_DIR, "training_results")
 STATIC_GESTURE_RESULTS_ROOT = os.path.join(
