fc6434aba20b0e05e8e847457b7e6ded96b3e11a
Andrey Shilkov <a.shilkov@3divi.com>
2023-04-15 15:30:03 +0500

    fixed type hints

--- static_gesture_classification/classification_results_dataframe.py
+++ static_gesture_classification/classification_results_dataframe.py
@@ -5,17 +5,17 @@ import pandas as pd
 @dataclass
 class ClassificationResultsDataframe(pd.DataFrame):
     @property
-    def image_path(self) -> str:
+    def image_path(self) -> pd.Series:
         ...
 
     @property
-    def ground_true(self) -> str:
+    def ground_true(self) -> pd.Series:
         ...
 
     @property
-    def prediction(self) -> str:
+    def prediction(self) -> pd.Series:
         ...
 
     @property
-    def prediction_score(self) -> float:
+    def prediction_score(self) -> pd.Series:
         ...
