229033252de6258e597b4995650c382fff364441
Andrey Shilkov <a.shilkov@3divi.com>
2023-04-20 01:09:42 +0500

    added utils for IOU computation and timing

--- general/utils.py
+++ general/utils.py
@@ -1,10 +1,11 @@
 import numpy as np
 import os
 import torch
-from typing import List, Optional, Dict
+from typing import List, Optional, Dict, Iterable, Tuple
 from general.datasets.read_meta_dataset import ReadMetaDataset
 from matplotlib.axes._axes import Axes
 import torchvision.transforms as tf
+import time
 
 
 class TorchNormalizeInverse:
@@ -21,6 +22,17 @@ class TorchNormalizeInverse:
 
     def __call__(self, tensor):
         return self.normalization_inverse(tensor.clone())
+
+
+def timing_decorator(func):
+    def wrapper(*arg, **kw):
+        t1 = time.time()
+        res = func(*arg, **kw)
+        t2 = time.time()
+        print(f"{func.__name__} took {t2 - t1} seconds")
+        return res
+
+    return wrapper
 
 
 def traverse_all_files(data_root: str) -> List[str]:
@@ -77,3 +89,35 @@ def plot_images_in_grid(axes: List[List[Axes]], images: List[np.ndarray]) -> Non
     for i in range(col_num):
         for j in range(row_num):
             axes[i][j].axis("off")
+
+
+def xyxy_box_area(xyxy_box: Iterable[float]) -> float:
+    x1, y1, x2, y2 = xyxy_box
+    widtdh = x2 - x1
+    height = y2 - y1
+    return widtdh * height
+
+
+def get_intervals_intersection(
+    interval1: Tuple[float, float], interval2: Tuple[float, float]
+) -> float:
+    start1, end1 = interval1
+    start2, end2 = interval2
+    iou = min(end2, end1) - max(start1, start2)
+    iou = max(iou, 0)
+    return iou
+
+
+def get_xyxy_boxes_iou(xyxy_box1: Iterable[float], xyxy_box2: Iterable[float]) -> float:
+    box1_x1, box1_y1, box1_x2, box1_y2 = xyxy_box1
+    box2_x1, box2_y1, box2_x2, box2_y2 = xyxy_box2
+    intersection_area = get_intervals_intersection(
+        interval1=(box1_x1, box1_x2), interval2=(box2_x1, box2_x2)
+    ) * get_intervals_intersection(
+        interval1=(box1_y1, box1_y2), interval2=(box2_y1, box2_y2)
+    )
+    area1 = xyxy_box_area(xyxy_box1)
+    area2 = xyxy_box_area(xyxy_box2)
+    union_area = area1 + area2 - intersection_area
+    iou = intersection_area / union_area
+    return iou
