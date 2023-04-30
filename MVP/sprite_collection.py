37e695c58d0b93a50329f9d591eb468fac9e2a21
Andrey Shilkov <a.shilkov@3divi.com>
2023-05-01 01:12:06 +0500

    added loading of new sprites

--- MVP/sprite_collection.py
+++ MVP/sprite_collection.py
@@ -2,6 +2,7 @@ import os
 from static_gesture_classification.static_gesture import StaticGesture
 from MVP.rock_paper_scissors_game.game_mechanics import RPSGameOption, EndGameResult
 from MVP.data_structures.game_kind import GameKind
+from MVP.memory_game.memory_game_result_kind import MemoryGameResultKind
 import arcade
 from typing import Dict
 
@@ -46,12 +47,61 @@ class SpriteCollection:
         self.menu_background_sprite: arcade.Sprite = arcade.Sprite(
             os.path.join(self.sprites_root, "Menu", "MenuBackground.png")
         )
+        self.menu_cards_with_rules = self.load_menu_game_cards(state="rules")
         self.closed_menu_cursor: arcade.Sprite = arcade.Sprite(
             os.path.join(self.sprites_root, "Menu", "closed_cursor.png")
         )
         self.open_menu_cursor: arcade.Sprite = arcade.Sprite(
             os.path.join(self.sprites_root, "Menu", "open_cursor.png")
         )
+        # MEMORY GAME
+        self.memory_game_preparation_bg: arcade.Sprite = arcade.Sprite(
+            os.path.join(self.sprites_root, "MemoryGame", "MemoryGamePreparation.png")
+        )
+        self.memory_game_memorization_bg: arcade.Sprite = arcade.Sprite(
+            os.path.join(
+                self.sprites_root, "MemoryGame", "MemoryGameMemorizationStage.png"
+            )
+        )
+        self.memory_game_results_bg: arcade.Sprite = arcade.Sprite(
+            os.path.join(self.sprites_root, "MemoryGame", "MemoryGameResults.png")
+        )
+        self.memory_game_gesture_demo_bg: arcade.Sprite = arcade.Sprite(
+            os.path.join(
+                self.sprites_root, "MemoryGame", "MemoryGameGesturesDemonstration.png"
+            )
+        )
+        self.memory_game_reults: Dict[MemoryGameResultKind, arcade.Sprite] = {
+            kind: arcade.Sprite(
+                filename=os.path.join(
+                    self.sprites_root, "MemoryGame", "results", f"{kind.name}.png"
+                )
+            )
+            for kind in MemoryGameResultKind
+        }
+        self.numbers_cards: Dict[int, arcade.Sprite] = {
+            number: arcade.Sprite(
+                os.path.join(
+                    self.sprites_root, "MemoryGame", "numbers", f"{number}.png"
+                )
+            )
+            for number in range(6)
+        }
+        self.not_active_gestures_cards_paths: Dict[
+            StaticGesture, str
+        ] = self.get_gestures_cards_paths(state="not_active")
+        self.active_gestures_cards_paths: Dict[
+            StaticGesture, str
+        ] = self.get_gestures_cards_paths(state="active")
+
+    def get_gestures_cards_paths(self, state: str) -> Dict[StaticGesture, str]:
+        gestures_cards_root = os.path.join(self.sprites_root, "MemoryGame", state)
+        gestures_cards_sprites: Dict[StaticGesture, str] = {}
+        for static_gesture in StaticGesture:
+            gestures_cards_sprites[static_gesture] = os.path.join(
+                gestures_cards_root, f"{static_gesture.name}.png"
+            )
+        return gestures_cards_sprites
 
     def load_getures_sprites(self, state: str) -> Dict[StaticGesture, arcade.Sprite]:
         gestures_root = os.path.join(self.sprites_root, "GesturesDetections", state)
